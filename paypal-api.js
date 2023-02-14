a51d3cfa56466c3dc8a71d7612ab9fc32830746f
Panos Mentzelopoulos <pmentzelopoulos@paypal.com>
2023-02-14 11:46:09 +0000

    fix error on body of verifyWebhook 3

--- paypal-api.js
+++ paypal-api.js
@@ -103,6 +103,10 @@ export async function check3DS(orderId) {
 export async function verifyWebhook(headersObj, bodyObj) {
   const accessToken = await generateAccessToken();
   const url = `${base}/v1/notifications/verify-webhook-signature`;
+  console.log("headers trans time: ", headersObj["paypal-transmission-time"]);
+  console.log("headers trans sig: ", headersObj["paypal-transmission-sig"]);
+  console.log("bodyObj: ", bodyObj);
+
   const response = await fetch(url, {
     method: "post",
     headers: {
